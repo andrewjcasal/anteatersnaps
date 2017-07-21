911a66df871bd0aa0be6511491deb2e9d25aaf09
Andrew Casal <andrewjcasal@gmail.com>
2017-07-21 12:01:53 -0700

    about page, templates

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.render('home');
+})
+
+app.get('/about', function (req, res) {
+  res.render('about');
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
